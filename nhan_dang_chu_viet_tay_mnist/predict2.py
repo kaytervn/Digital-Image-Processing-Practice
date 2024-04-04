1722f5f9f68504f86b524ce0f0fb93362e803b21
KayterCantFixBug <kienductrong@gmail.com>
2024-04-04 10:48:14 +0700

    c

--- nhan_dang_chu_viet_tay_mnist/predict2.py
+++ nhan_dang_chu_viet_tay_mnist/predict2.py
@@ -21,20 +21,27 @@ model.compile(
 
 # reshape
 X_test = X_test.reshape((10000, 28, 28, 1))
-sample = X_test[2000]
-cv2.imshow("Digit", sample)
+
+# Tạo 100 số nguyên ngẫu nhiên nằm trong phạm vi [0, 9999]
+index = np.random.randint(0, 9999, 100)
+sample = np.zeros((100, 28, 28, 1))
+for i in range(0, 100):
+    sample[i] = X_test[index[i]]
+
+# Tạo ảnh để xem
+image = np.zeros((10 * 28, 10 * 28), np.uint8)
+k = 0
+for i in range(0, 10):
+    for j in range(0, 10):
+        image[i * 28 : (i + 1) * 28, j * 28 : (j + 1) * 28] = sample[k, :, :, 0]
+        k += 1
+
+cv2.imshow("Image", image)
 
 # normalize
-sample = sample / 255.0
+data = sample / 255.0
 
 # cast
-sample = sample.astype("float32")
-
-# add dimension to image head
-sample = np.expand_dims(sample, axis=0)
-
-ket_qua = model.predict(sample, verbose=0)
-chu_so = np.argmax(ket_qua[0])
-print("Kết quả nhận dạng là: ", chu_so)
+data = data.astype("float32")
 
 cv2.waitKey(0)
